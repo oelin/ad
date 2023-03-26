8d8f734a525531bacc1af04cb92b894e39c54dc3
kale <42823429+oelin@users.noreply.github.com>
2023-03-26 20:21:31 +0100

    Create ad.py

---
+++ ad.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass 
+
+@dataclass
+class Variable:
+    data: np.array
+    grad: np.array
+    requires_grad: bool = False 
+    back: callable = None
+    
+    def backward(self, grad = 1.) -> None:
+        self.grad += grad 
+        self.back and self.back()
+        self.grad *= self.requires_grad
+
+@dataclass
+class Function:
+    def __call__(self, *variables) -> Variable:
+        result = self.forward(*variables)
+
+        def back():
+            self.backward(*variables, result.grad)
+
+            for variable in variables:
+                variable.backward(0.)
+        
+        result.back = back 
+        return result
